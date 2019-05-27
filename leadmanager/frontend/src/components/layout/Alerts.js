a9ad3e7c8fe41f6c8e211e0328746b323759d660
Kevin <stradtkt22@gmail.com>
2019-05-27 12:50:39 -0400

    Added messages for when there are errors on the form

--- leadmanager/frontend/src/components/layout/Alerts.js
+++ leadmanager/frontend/src/components/layout/Alerts.js
@@ -1,13 +1,36 @@
-import React, {Component, Fragment} from 'react'
+import React, {Component, Fragment} from 'react';
 import {withAlert} from 'react-alert';
+import {connect} from 'react-redux';
+import PropTypes from 'prop-types';
+
 
 
 export default class Alerts extends Component {
-    componentDidMount() {
-        this.props.alert.show('It Works');
+    componentDidUpdate(prevProps) {
+        const {error, alert} = this.props;
+        if(error !== prevProps.error) {
+            if(error.msg.name) {
+                alert.error(`Name: ${error.msg.name.join()}`);
+            }
+            if(error.msg.email) {
+                alert.error(`Email: ${error.msg.email.join()}`);
+            }
+            if(error.msg.message) {
+                alert.error(`Message: ${error.msg.message.join()}`);
+            }
+        }
     }
     render() {
         return <Fragment/>;
     }
 }
-export default withAlert(Alerts);
+
+Alerts.propTypes = {
+    error: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+    error: state.errors
+});
+
+export default connect(mapStateToProps)(withAlert(Alerts));
